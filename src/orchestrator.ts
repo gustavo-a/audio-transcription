6252fdfa1c7c1830e8c46c373ceb0f21e0730e14
Gustavo Rocha <gustavo@onserp.com.br>
2023-09-03 13:05:03 -0300

    separate files

---
+++ src/orchestrator.ts
@@ -0,0 +1,40 @@
+import readline from 'node:readline/promises'
+
+import transcribe from '@/transcribe'
+import createOutput from '@/output'
+
+//
+const init = async () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  try {
+    // NOTE: the second parameter (the timeout) is optional.
+    const filePath = await rl.question('Path to the audio file: ')
+    const prompt = await rl.question('Prompt: ')
+    const language = await rl.question('Language: ')
+    const temperature = await rl.question('Temperature: ')
+    const responseFormat = await rl.question('Output format: ')
+
+    const transcribedText = await transcribe({
+      filePath,
+      prompt,
+      language,
+      temperature,
+      response_format: responseFormat as TranscribeOptions['response_format']
+    })
+
+    if (!transcribedText) return
+
+    createOutput({
+      content: transcribedText,
+      outputType: responseFormat as TranscribeOptions['response_format']
+    })
+  } finally {
+    rl.close()
+  }
+}
+
+export default init
